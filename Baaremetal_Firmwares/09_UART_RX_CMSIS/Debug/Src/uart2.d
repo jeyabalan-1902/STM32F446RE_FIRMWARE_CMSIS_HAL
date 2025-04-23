22ed9e550aadf08ecc787283836b5c4b5a1aa116
jeyabalan-1902 <kjeyabalan19@outlook.com>
2025-04-23 23:21:03 +0530

    uart Tx and Rx drivers

---
+++ Baaremetal_Firmwares/09_UART_RX_CMSIS/Debug/Src/uart2.d
@@ -0,0 +1,18 @@
+Src/uart2.o: ../Src/uart2.c ../Inc/uart2.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/core_cm4.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../Inc/uart2.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/core_cm4.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/cmsis_version.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/kjeya/Documents/STM32F446RET6_baremetal_docs/Baaremetal_Firmwares/MCU_Headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
---
+++ Baaremetal_Firmwares/09_UART_RX_CMSIS/Inc/uart2.h
@@ -0,0 +1,18 @@
+/*
+ * uart2.h
+ *
+ *  Created on: Apr 23, 2025
+ *      Author: kjeyabalan
+ */
+
+#ifndef UART2_H_
+#define UART2_H_
+
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+void UART2_Init(void);
+char uart2_receive(void);
+
+
+#endif /* UART2_H_ */
