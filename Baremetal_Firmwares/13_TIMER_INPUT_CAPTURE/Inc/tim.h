0e392581867fb8853d311cb4079e51f4d05839e3
jeyabalan-1902 <kjeyabalan19@outlook.com>
2025-05-08 18:35:11 +0530

    Timer2 PA5 input capture mode driver

--- Baremetal_Firmwares/13_TIMER_INPUT_CAPTURE/Inc/tim.h
+++ Baremetal_Firmwares/13_TIMER_INPUT_CAPTURE/Inc/tim.h
@@ -10,6 +10,8 @@
 
 void tim2_1Hz_init(void);
 void tim2_PA5_output_compare(void);
+void tim3_input_capture(void);
 
+#define SR_CC1IF (1U << 1)
 #define SR_UIF   (1U << 0)
 #endif /* TIM_H_ */
--- Baremetal_Firmwares/13_TIMER_INPUT_CAPTURE/Src/tim.c
+++ Baremetal_Firmwares/13_TIMER_INPUT_CAPTURE/Src/tim.c
@@ -11,6 +11,7 @@
 #include "tim.h"
 
 #define TIM2EN     (1U << 0)
+#define TIM3EN     (1U << 1)
 #define CR1_CEN    (1U << 0)
 
 #define OCM_TOGGLE ((1U << 4) | (1U << 5))
@@ -71,5 +72,26 @@ void tim2_PA5_output_compare(void)
 	TIM2->CNT = 0;
 	/*enable timer*/
 	TIM2->CR1 |= CR1_CEN;
+}
+
+void tim3_input_capture(void)
+{
+	/*enable clock access for GPIOA*/
+	RCC->AHB1ENR |= GPIOAEN;
+	/*Set PA6 mode to alternate function*/
+	GPIOA->MODER |= (1U << 13);
+	GPIOA->MODER &= ~(1U << 12);
+	/*set PA6 alternate function type to TIM_CH1 (AF02).*/
+	GPIOA->AFR[0] |= (1U << 25);
+	/*Enable clock acces for tim3*/
+	RCC->APB1ENR |= TIM3EN;
+	/*set prescaler*/
+	TIM3->PSC = 16000 -1;
+	/*set CH1 to input capture mode*/
+	TIM3->CCMR1 |= (1U << 0);
+	/*set CH1 to capture at rising edge*/
+	TIM3->CCER = (1U << 0);
+	/* Enable TIM3*/
+	TIM3->CR1 |= CR1_CEN;
 }
 
